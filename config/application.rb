280ea56c0bb70675c487bdf3b861b3caefd58ac5
Dmytro Khmelenko <d.khmelenko@gmail.com>
2025-03-26 19:05:42 +0100

    Adjust controller

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module SecureApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+
+    config.session_store :cookie_store, key: 'api_app_session'
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
